f384bf84cf64f0cb6471409ec07a217e43d7d1bd
Hrustik-app <evgeniiturbou@mail.ru>
2025-11-19 00:54:38 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif', textAlign: 'center' }}>
+      <h1>Прокси-сервер готов</h1>
+      <p>Для доступа к API используйте /api/investing</p>
+    </div>
+  );
+}
